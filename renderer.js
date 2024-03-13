05a9b3a4e3ce026b432d605c7c973c2a93575e1e
Srimani Teja Ejjagiri <123293913+SrimaniTeja@users.noreply.github.com>
2024-03-13 16:23:22 +0530

    Add files via upload

---
+++ renderer.js
@@ -0,0 +1,48 @@
+/**
+ * This file is loaded via the <script> tag in the index.html file and will
+ * be executed in the renderer process for that window. No Node.js APIs are
+ * available in this process because `nodeIntegration` is turned off and
+ * `contextIsolation` is turned on. Use the contextBridge API in `preload.js`
+ * to expose Node.js functionality from the main process.
+ */
+let imgInp = document.getElementById("imgInp")
+let blah = document.getElementById("blah")
+let img = new Image()
+imgInp.onchange = evt => {
+    let [file] = imgInp.files
+    if (file) {
+        blah.src = file.path;
+        img.src = blah.src;
+        console.log(blah.naturalWidth, blah.naturalHeight);
+        
+    }
+}
+function download() {
+    let canvas = document.createElement("canvas");
+    canvas.width = img.naturalWidth;
+    canvas.height = img.naturalHeight;
+    let ctx = canvas.getContext("2d");
+    ctx.filter = "invert(0%)";
+    ctx.drawImage(img, 0, 0);
+    let a = document.createElement("a");
+    a.href = canvas.toDataURL();
+    a.download = "image.png";
+    a.click();
+}
+function updateHistory() {
+    historyIndex++;
+    history[historyIndex] = canvs.value;
+    history.length = historyIndex + 1;
+}
+function undo(){
+    if (historyIndex > 0) {
+        historyIndex--;
+        canvas.value = history[historyIndex];
+    }
+}
+function redo(){
+    if (historyIndex < history.length - 1) {
+        historyIndex++;
+        canvas.value = history[historyIndex];
+    }
+}
